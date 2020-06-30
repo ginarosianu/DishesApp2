2b15edb8d57e71ed00f19ef1cf1a7ae238aeb16b
Virginia Rosianu <gina.rosianu@gmail.com>
2020-07-01 00:29:19 +0300

    Add project files.

---
+++ DishesApp/Controllers/DishesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DishRecipeApp.Models;
+using DishesApp.Data;
+
+namespace DishesApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DishesController : ControllerBase
+    {
+        private readonly DishesAppContext _context;
+
+        public DishesController(DishesAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Dishes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Dish>>> GetDish()
+        {
+            return await _context.Dish.ToListAsync();
+        }
+
+        // GET: api/Dishes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Dish>> GetDish(long id)
+        {
+            var dish = await _context.Dish.FindAsync(id);
+
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            return dish;
+        }
+
+        // PUT: api/Dishes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDish(long id, Dish dish)
+        {
+            if (id != dish.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(dish).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DishExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Dishes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Dish>> PostDish(Dish dish)
+        {
+            _context.Dish.Add(dish);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDish", new { id = dish.Id }, dish);
+        }
+
+        // DELETE: api/Dishes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Dish>> DeleteDish(long id)
+        {
+            var dish = await _context.Dish.FindAsync(id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            _context.Dish.Remove(dish);
+            await _context.SaveChangesAsync();
+
+            return dish;
+        }
+
+        private bool DishExists(long id)
+        {
+            return _context.Dish.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ DishesApp/Data/DishesAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DishRecipeApp.Models;
+
+namespace DishesApp.Data
+{
+    public class DishesAppContext : DbContext
+    {
+        public DishesAppContext (DbContextOptions<DishesAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DishRecipeApp.Models.Dish> Dish { get; set; }
+    }
+}
